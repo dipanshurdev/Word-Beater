d9adc14c8922e4ab21c73dd359e912a6776db490
Aman <dv451197@gmail.com>
2024-03-26 20:49:20 +0530

    let's go

---
+++ speedType.js
@@ -0,0 +1,125 @@
+window.addEventListener('load', initialize);
+
+// Levels
+const levels = {
+    easy: 5,
+    medium: 3,
+    hard: 2
+}
+// change levels
+const currentLevel = levels.easy;
+
+// Global Veriables
+let time = currentLevel;
+let score = 0;
+let isPlaying; 
+
+// DOM Elements
+
+const wordInput = document.querySelector('#input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+const btnEasy = document.querySelector('.easy')
+const btnMedium = document.querySelector('.easy')
+const btnHard = document.querySelector('.hard')
+// Words Array
+
+const words = [
+    'hat',
+    'river',
+    'status',
+    'generate',
+    'stubborn',
+    'cocktil',
+    'mokey',
+    'elephant',
+    'establishment',
+    'congratulation',
+    'investigate',
+    'conquror',
+    'jacket',
+    'jocker',
+    'master',
+    'relationship',
+    'greatfullness',
+    'buggati',
+    'lamborghini',
+    'aeroplane',
+    'magic',
+    'defination',
+    'courage',
+    'document',
+    'javascript'
+];
+
+// initializing game
+function initialize() {
+    // show numbers of seconds in UI
+    seconds.innerHTML = currentLevel;
+    // Load word form Game
+    showWord(words);
+    // start mathcing input
+    wordInput.addEventListener('input', startMatch)
+    // call countdown every second
+    setInterval(countdown, 1000);
+    // check game status
+    setInterval(checkStatus, 500);
+}
+
+// start match function
+function startMatch() {
+    if (matchWords()) {
+        isPlaying = true;
+        time = currentLevel + 1;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
+    // if score is -1 display 0;
+    if (score === -1) {
+        scoreDisplay.innerHTML = 0;
+    } else {
+        scoreDisplay.innerHTML = score;
+    }
+}
+
+// match currentWord to wordInput
+function matchWords() {
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!!!!'
+        return true;
+    } else {
+        message.innerHTML = 'Hurry Up!'
+        return false;
+    }
+}
+
+// Pick & Show random word
+function showWord(words) {
+    // Generate random array index
+    const randomIndex = Math.floor(Math.random() * words.length);
+    // output the random word
+    currentWord.innerHTML = words[randomIndex]
+}
+
+// countdown timer
+function countdown() {
+    if (time > 0) {
+        time--;
+    } else {
+        isPlaying = false;
+    }
+    // show time 
+    timeDisplay.innerHTML = time;
+}
+
+// check game status function
+function checkStatus() {
+    if (!isPlaying && time === 0) {
+        message.innerHTML = "Game is Over!!!";
+        score = -1;
+    }
+}
